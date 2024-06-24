62644ce5176a985761077a8885999c1d5daba846
ivobro <ivo.boychi@gmail.com>
2024-06-24 20:53:58 +0000

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+
+(function ($) {
+  "use strict";
+  
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+    });
+    
+
+    // PARALLAX EFFECT
+  
+
+
+    // MAGNIFIC POPUP
+    $('.image-popup').magnificPopup({
+        type: 'image',
+        removalDelay: 300,
+        mainClass: 'mfp-with-zoom',
+        gallery:{
+          enabled:true
+        },
+        zoom: {
+        enabled: true, // By default it's false, so don't forget to enable it
+
+        duration: 300, // duration of the effect, in milliseconds
+        easing: 'ease-in-out', // CSS transition easing function
+
+        // The "opener" function should return the element from which popup will be zoomed in
+        // and to which popup will be scaled down
+        // By defailt it looks for an image tag:
+        opener: function(openerElement) {
+        // openerElement is the element on which popup was initialized, in this case its <a> tag
+        // you don't need to add "opener" option if this code matches your needs, it's defailt one.
+        return openerElement.is('img') ? openerElement : openerElement.find('img');
+        }
+      }
+    });
+
+/*
+    // SMOOTH SCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    }); 
+
+*/
+  // Scroll Activated Number Counter
+  var a = 0;
+  $(window).scroll(function() {
+
+    var oTop = $('.about-info').offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() > oTop) {
+      $('.count').each(function() {
+        var $this = $(this),
+          countTo = $this.attr('data-counter-time');
+        $({
+          countNum: $this.text()
+        }).animate({
+            countNum: countTo
+          },
+          {
+            duration: 2000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+            }
+          });
+      });
+      a = 1;
+    }
+  });
+})(jQuery);
+
